efbd7281ccb2f9a06badb0a635ae9775912a42c3
Morozova Valeriya <sandy.lera@yandex.ru>
2022-12-12 20:16:38 +0300

    исправление ошибок

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,26 @@
-//открытие попапа
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
+const closeButton = document.querySelector('.popup__close-button');
+const formElement = document.querySelector('.form');
+const nameInput = document.querySelector('.form__field_type_name');
+const jobInput = document.querySelector('.form__field_type_job');
+const profileName = document.querySelector('.profile__name');
+const profileCaption = document.querySelector('.profile__description');
+//открытие попапа
 function openPopup() {
     popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileCaption.textContent;
 }
 editButton.addEventListener('click', openPopup);
 
-//закрытие попапа
-const closeButton = document.querySelector('.popup__close-button');
+//закрытие попапа 
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
 closeButton.addEventListener('click', closePopup);
 
 //редактирование информации в профиле и отправка формы
-const formElement = document.querySelector('.form');
-const nameInput = document.querySelector('.form__field_name-input');
-const jobInput = document.querySelector('.form__field_job-input');
-const profileName = document.querySelector('.profile__name');
-const profileCaption = document.querySelector('.profile__description');
 function handleFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
